5e053e49d7bec16ef7b90d62c828bd0a3f0a34ff
Orban Botond <orbanbotond@gmail.com>
2018-11-21 17:32:27 +0200

    The Author Repository is speced well.

--- lib/app/repositories/author_repository.rb
+++ lib/app/repositories/author_repository.rb
@@ -1,2 +1,21 @@
 class AuthorRepository < Hanami::Repository
+  associations do
+    has_many :books
+  end
+
+  def create_with_books(data)
+    assoc(:books).create(data)
+  end
+
+  def find_with_books(id)
+    aggregate(:books).where(id: id).map_to(Author).one
+  end
+
+  def add_book(author, data)
+    assoc(:books, author).add(data)
+  end
+
+  # def remove_book(author, id)
+  #   assoc(:books, author).remove(id)
+  # end
 end
--- spec/app/repositories/author_repository_spec.rb
+++ spec/app/repositories/author_repository_spec.rb
@@ -1,3 +1,57 @@
 RSpec.describe AuthorRepository, type: :repository do
-  # place your tests here
+  let(:repo) { described_class.new }
+
+  context '#find' do
+    let!(:author) { repo.create(name: 'Boti') }
+
+    specify 'The record' do
+      expect(repo.find(author.id)).to_not be_nil
+    end
+  end
+
+  context '#create_with_books' do
+    let(:params) { { name: "Alexandre Dumas", books: [{title: "The Count of Montecristo"}] } }
+
+    specify 'The books will be created' do
+      author = repo.create_with_books(params)
+
+      expect(author.id).to_not be_nil
+      expect(author.books).to_not be_nil
+    end
+  end
+
+  context '#find_with_books' do
+    let(:params) { {name: "Alexandre Dumas", books: [{title: "The Count of Montecristo"}]} }
+
+    specify 'The books will be created' do
+      author = repo.create_with_books(params)
+
+      author = repo.find_with_books(author.id)
+      expect(author.books).to_not be_nil
+    end
+  end
+
+  context '#add_book' do
+    let!(:author) { repo.create(name: 'Boti') }
+    let(:params) { {title: "The Count of Montecristo"} }
+
+    specify 'The book will be added' do
+      book = repo.add_book(author, params)
+
+      expect(book.title).to_not be_nil
+    end
+  end
+
+  # context '#remove_book' do
+  #   let(:params) { { name: "Alexandre Dumas", books: [{title: "The Count of Montecristo"}] } }
+  #   let!(:author) { repo.create_with_books(params) }
+
+  #   specify 'The book will be removed' do
+  #     puts author.books
+  #     puts author.books.first.id
+
+  #     # repo.remove_book(author, author.books.first.id)
+  #     # expect(repo.find_with_books(author.id).books).to be_nil
+  #   end
+  # end
 end
