a84e027a4e0cb4cff8ffae600881c6736580e403
Orban Botond <orbanbotond@gmail.com>
2018-11-22 02:55:05 +0200

    The users are added.

--- lib/app/repositories/comment_repository.rb
+++ lib/app/repositories/comment_repository.rb
@@ -1,7 +1,6 @@
 class CommentRepository < Hanami::Repository
   associations do
     belongs_to :story
-    # belongs_to :user
     belongs_to :user, as: :commenter
   end
 end
--- lib/app/repositories/story_repository.rb
+++ lib/app/repositories/story_repository.rb
@@ -2,14 +2,15 @@ class StoryRepository < Hanami::Repository
   associations do
     belongs_to :user, as: :author
     has_many :comments
-    has_many :users, through: :comments, as: :commenters
+    has_many :users, through: :comments
+    # has_many :users, through: :comments, as: :commenters
   end
 
   def find_with_comments(id)
     aggregate(:author, comments: :commenter).where(id: id).map_to(Story).one
   end
 
-  def find_with_commenters(id)
-    aggregate(:users).where(id: id).map_to(Story).one
-  end
+  # def find_with_commenters(id)
+  #   aggregate(:commenters).where(id: id).map_to(Story).one
+  # end
 end
