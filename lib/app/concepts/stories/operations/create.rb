1db70746635265d494bcfb800813098b04fab2d1
Orban Botond <orbanbotond@gmail.com>
2018-11-29 13:05:16 +0200

    The create operation is speced.

--- lib/app/concepts/stories/operations/create.rb
+++ lib/app/concepts/stories/operations/create.rb
@@ -3,6 +3,14 @@
 module Stories
   module Operations
     class Create < Trailblazer::Operation
+      step Model(::Story, :new)
+      # step Contract::Build(constant: Stories::Contracts::Create)
+      # step Contract::Validate()
+      # step :persist
+
+      # def persist(options, params:, **)
+      #   option['model'] = StoryRepository.new.create(params)
+      # end
     end
   end
 end
--- spec/app/concepts/stories/operations/create_spec.rb
+++ spec/app/concepts/stories/operations/create_spec.rb
@@ -1,6 +1,41 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 describe Stories::Operations::Create do
+  let(:repo) { StoryRepository.new }
+  let(:options) { {} }
+  let(:user) { Fabricate.create(:user) }
+  let(:params) { Fabricate.attributes_for(:story).merge user_id: user.id }
+  let(:result) { described_class.call(params, options) }
+
+  context "negative cases" do
+    context "missing params" do
+      [:text, :user_id].each do |field|
+        let(:params) { super().reject{ |k| k == field } }
+
+        it "returns failure and sets result.contract.default" do
+          expect(result.failure?).to be_truthy
+          expect(result["result.contract.default"].failure?).to be_truthy
+        end
+      end
+    end
+
+    context "user_id is nonexistent" do
+      let(:params) { super().merge(user_id: -1) }
+
+      it "returns failure and sets result.contract.default" do
+        expect(result.failure?).to be_truthy
+        expect(result["result.fk_reference.default"].failure?).to be_truthy
+        expect(result["result.fk_reference.key"]).to eq(:user_id)
+      end
+    end
+  end
+
+  context "positive case" do
+    it "saves the entity properly details" do
+      expect do
+        expect(result["model"].text).to eq(params[:text])
+        expect(result["model"].user_id).to eq(params[:user_id])
+      end.to change { repo.all.count }.by(1)
+    end
+  end
 end
